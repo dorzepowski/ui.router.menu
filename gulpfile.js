401a8104c9bd2232e61a8ba5978bbc119c48d08f
Damian Orzepowski <damian.orzepowski@docusafe.pl>
2016-09-27 14:49:01 +0200

    Create server to serve for testing, create min version

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ var conf = {
     jsDest: "ui-router-menu-extension",
     jsDevFile: function () {
         return this.jsDest + ".js"
+    },
+    jsProdFile: function () {
+        return this.jsDest + ".min.js"
     }
 };
 
@@ -17,6 +20,8 @@ const del = require('del');
 const watch = require('gulp-watch');
 const print = require('gulp-print');
 const strip = require('gulp-strip-comments');
+const uglify = require('gulp-uglify');
+const webserver = require('gulp-webserver');
 
 gulp.task('clean', function () {
     return del([conf.dest], {dot: true});
@@ -29,14 +34,32 @@ gulp.task("build:dev", function(){
        .pipe(gulp.dest(conf.dest));
 });
 
-gulp.task("watch", function(){
-    gulp.watch(conf.jsSrc(), function(file){
-        gulp.src(file.path)
-            .pipe(print(modified));
-        gulp.run("build:dev")
-    });
+gulp.task("build:prod", function () {
+    return gulp.src(conf.jsSrc())
+        .pipe(concat(conf.jsProdFile()))
+        .pipe(uglify())
+        .pipe(gulp.dest(conf.dest));
 });
 
-function modified(file) {
-    return "File (" + file + ") modified -> rebuilding";
-}
+gulp.task("watch", function(){
+    var watcher = gulp.watch(conf.jsSrc(), ["build:dev", "build:prod"]);
+    watcher.on("add", logModified);
+    watcher.on("change", logModified);
+    watcher.on("delete", logModified);
+
+
+    function logModified(event) {
+        console.info("File (" + event.path + ") " + event.type + " -> rebuilding");
+    }
+});
+
+
+gulp.task('serve', function () {
+    gulp.src(conf.dest)
+        .pipe(webserver({
+            port: '20000'
+        }));
+});
+
+
+gulp.task("start", ["build:dev", "build:prod", "serve", "watch"]);
